d8725309fd173b3089b5d6b9e7fbde9a8e9a805f
luis_felipes14 <luisfelipestututi@gmail.com>
2023-05-14 15:53:33 -0300

    feat: Add RelatoryView and Context Relatory.js

--- frontend/droneseta/src/utils/relatory.js
+++ frontend/droneseta/src/utils/relatory.js
@@ -15,18 +15,98 @@ function RelatoryProvider({ children }) {
             {
                 id: 1,
                 title: "Clientes Cadastrados",
+                relCabecalho: [
+                    {
+                        titulo: "Código",
+                        formato: "Integer"
+                    },
+                    {
+                        titulo: "Cliente",
+                        formato: "String"
+                    },
+                ],
+                relDados: [
+                    [1, "Luis Felipe da Silva"],
+                    [2, "Murilo Goedert"],
+                    [3, "Gabriel Dolzan"],
+                    [4, "Lucas Dolsan"],
+                    [5, "Angelina Dolzan"],
+                ]
             },
             {
                 id: 2,
                 title: "Produtos mais vendidos",
+                relCabecalho: [
+                    {
+                        titulo: "Código",
+                        formato: "Integer"
+                    },
+                    {
+                        titulo: "Produto",
+                        formato: "String"
+                    },
+                    {
+                        titulo: "Qtd. Vendas",
+                        formato: "Integer"
+                    },
+                ],
+                relDados: [
+                    [1, "Camiseta Slikpnot", 54],
+                    [2, "Camiseta Linkin Park", 46],
+                    [3, "Camiseta System of a Down", 35],
+                    [4, "Camiseta Three Days Grace", 21],
+                    [5, "Camiseta Bring Me The Horizon", 12],
+                ]
             },
             {
                 id: 3,
                 title: "Pedidos Pendentes",
+                relCabecalho: [
+                    {
+                        titulo: "Pedido",
+                        formato: "Integer"
+                    },
+                    {
+                        titulo: "Cliente",
+                        formato: "String"
+                    },
+                    {
+                        titulo: "R$ Pedido",
+                        formato: "Double"
+                    },
+                ],
+                relDados: [
+                    [1, "Luis Felipe da Silva", 49.99],
+                    [2, "Murilo Goedert", 90.00],
+                    [3, "Gabriel Dolzan", 25.50],
+                    [4, "Lucas Dolsan", 127.99],
+                    [5, "Angelina Dolzan", 317.00],
+                ]
             },
             {
                 id: 4,
                 title: "Pedidos Finalizados",
+                relCabecalho: [
+                    {
+                        titulo: "Pedido",
+                        formato: "Integer"
+                    },
+                    {
+                        titulo: "Cliente",
+                        formato: "String"
+                    },
+                    {
+                        titulo: "R$ Pedido",
+                        formato: "Double"
+                    },
+                ],
+                relDados: [
+                    [1, "Luis Felipe da Silva", 49.99],
+                    [2, "Murilo Goedert", 90.00],
+                    [3, "Gabriel Dolzan", 25.50],
+                    [4, "Lucas Dolsan", 127.99],
+                    [5, "Angelina Dolzan", 317.00],
+                ]
             },
         ];
     }
--- frontend/droneseta/src/views/RelatoryView.js
+++ frontend/droneseta/src/views/RelatoryView.js
@@ -13,11 +13,74 @@ function RelatoryView() {
     const params = useParams();
     const relatory = ctxRelatory.getRelatory(params.tipo);
 
+    // Para saber quantas colunas tera na grid
+    function getColunas() {
+        let colunas = relatory.relCabecalho.length;
+
+        let estilo = {
+            gridTemplateColumns: "repeat(" + colunas + ", 1fr)",
+        }
+
+        return estilo;
+    }
+
+    // Para recuperar o tipo de dado da coluna
+    function getTipoDado(index) {
+        let formato = relatory.relCabecalho[index].formato;
+
+        return formato;
+    }
+
+    // Para alterar o estilo de acordo com o tipo de dado
+    // Altera o tamanho do campo de acordo com o tipo
+    function getEstiloTipoDado(index) {
+        let formato = getTipoDado(index);
+        let estilo = [];
+
+        if (formato === "Integer") {
+            estilo = {
+                width: "10vh",
+            }
+        } else if (formato === "Double") {
+            estilo = {
+                width: "25vh",
+            }
+        } else if (formato === "String") {
+            estilo = {
+                width: "40vh",
+            }
+        }
+
+        return estilo;
+    }
+
     return (
         <div>
             <MyHeader />
             <div id="mainRelatory">
                 <h2> { relatory.title } </h2>
+                <div id="divRelatory">
+                    <div id="divCabecalho" style={ getColunas() }>
+                        { relatory.relCabecalho.map((cabecalho, index) => (
+                            <div key={ index } style={ getEstiloTipoDado(index) }>
+                                <span>{ cabecalho.titulo }</span>
+                            </div>
+                        )) }
+                    </div>
+                    <div id="divDados" >
+                        { relatory.relDados.map((dado, index) => (
+                            <div className="divRowDado" style={ getColunas() }>
+                                { dado.map((item, chave) => (
+                                    <div className="divItem" key={ chave } style={ getEstiloTipoDado(chave) }>
+                                        {/* Se o valor for double, converte para reais */}
+                                        { getTipoDado(chave) === "Double" && <span>{ item.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }</span> }
+                                        { getTipoDado(chave) !== "Double" && <span>{ item }</span> }
+                                    </div>
+                                )) }
+                            </div>
+                        )) }
+                    </div>
+                </div>
             </div>
         </div>
     );
